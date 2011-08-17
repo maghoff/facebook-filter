9e03c658df43184eea7a62357a073bbd5ad1b717
Magnus Hoff <maghoff@gmail.com>
2011-08-17 22:20:38 +0200

    Complete-ish

--- filter.py
+++ filter.py
@@ -2,6 +2,10 @@
 
 # Remember to sudo apt-get install python-vobject
 
+import cgi
+import cgitb
+cgitb.enable()
+
 import vobject, urllib2, cgi
 
 
@@ -24,23 +28,35 @@ def execute_filter(src_uri, acceptable):
 
 		# This is the filtering part
 		rsvp = event.partstat.value
-		if rsvp in ACCEPTABLE_PARTSTATUSES:
-			#outcal.contents['vcard'].append(event)
+		if rsvp in acceptable:
 			outcal.add(event)
 
 	print "Content-Type: text/calendar;encoding=utf-8"
 	print
-	print outcal.serialize().decode('utf-8')
+	print outcal.serialize()
 
 
 def print_form():
-	print """<!DOCTYPE html>
+	print """Content-Type: text/html;encoding=utf-8
+
+<!DOCTYPE html>
 <html><head>
 <title>Facebook events filter</title>
 </head><body>
+<h1>Facebook events filter</h1>
+
 <form method="GET">
-<input name="src" type="text"></input>
-<input type="submit"></input>
+<label for="src">Your Facebook event feed URI:</label><br/>
+<input name="src" type="text" size="90" placeholder="http://www.facebook.com/ical/u.php?uid=...&key=..."></input><br/>
+
+<p>Include only events with the following RSVP status:</p>
+<p>
+<input name="accept" type="checkbox" value="ACCEPTED" checked></input>Attending<br/>
+<input name="accept" type="checkbox" value="TENTATIVE" checked></input>Maybe attending<br/>
+<input name="accept" type="checkbox" value="DECLINED"></input>Not attending<br/>
+<input name="accept" type="checkbox" value="NEEDS-ACTION" checked></input>Waiting for reply<br/>
+</p>
+<input type="submit" value="Get filtered feed"></input>
 </form>
 </body></html>
 """
@@ -50,7 +66,7 @@ def main(argv):
 	form = cgi.FieldStorage()
 
 	if 'src' in form:
-		execute_filter(form['src'].value, ['ACCEPTED'])
+		execute_filter(form['src'].value, form.getlist('accept'))
 	else:
 		print_form()
 
