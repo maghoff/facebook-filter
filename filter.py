779dd491ce939b1701f09fb3f5d3acaad26b6b36
Magnus Hoff <maghoff@gmail.com>
2011-08-24 00:42:51 +0200

    Restored basic functionality and added js-functionality. Should now be useable *crosses fingers*

--- filter.py
+++ filter.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-# Remember to sudo apt-get install python-vobject
-
 #import cgi; import cgitb; cgitb.enable()
 
 import vobject, urllib2, cgi
@@ -37,73 +35,23 @@ def execute_filter(src_uri, acceptable):
 	print
 	print outcal.serialize()
 
+	return 0
 
-def print_form():
-	print """Content-Type: text/html;encoding=utf-8
 
-<!DOCTYPE html>
-<html><head>
-<title>Facebook events filter</title>
-<style type="text/css">
-input {
-	border: 1px solid black;
-	background: #EEF;
-}
-
-.tip {
-	font-family: "Helvetica", "Arial", sans-serif;
-	font-size: 70%;
-	border: 1px solid #777;
-	background-color: #FFC;
-	padding: 4px;
-	margin-top: 1px;
-	width: 25em;
-	color: #555;
-}
-
-.footer {
-	font-family: "Helvetica", "Arial", sans-serif;
-	font-size: 70%;
-	border-top: 1px solid #777;
-	color: #555;
-	width: 25em;
-	padding: 4px;
-	margin-top: 2em;
-}
-
-</style>
-</head><body>
-<h1>Facebook events filter</h1>
-
-<form method="GET">
-<label for="src">Your Facebook event feed URI:</label><br/>
-<input name="src" type="text" size="90" placeholder="webcal://www.facebook.com/ical/u.php?uid=...&key=..."></input><br/>
-
-<div class="tip">You can find your Facebook event feed URI by clicking the "Export"-link on <a href="https://www.facebook.com/events/">your events page</a></div>
-
-<p>Include only events with the following RSVP statuses:<br/>
-<input name="accept" type="checkbox" value="ACCEPTED" checked></input>Attending<br/>
-<input name="accept" type="checkbox" value="TENTATIVE" checked></input>Maybe attending<br/>
-<input name="accept" type="checkbox" value="DECLINED"></input>Not attending<br/>
-<input name="accept" type="checkbox" value="NEEDS-ACTION" checked></input>Waiting for reply<br/>
-</p>
-<input type="submit" value="Get filtered feed"></input>
-</form>
-
-<div class="footer">Go contribute to the <a href="https://bitbucket.org/maghoff/facebook-filter">source code</a>!</div>
-</body></html>
-"""
+def emit_error():
+	print "Content-Type: text/plain"
+	print ""
+	print "Missing src query parameter. Try again"
+	return 1
 
 
 def main(argv):
 	form = cgi.FieldStorage()
 
 	if 'src' in form:
-		execute_filter(form['src'].value, form.getlist('accept'))
+		return execute_filter(form['src'].value, form.getlist('accept'))
 	else:
-		print_form()
-
-	return 0
+		return emit_error()
 
 
 if __name__=='__main__':
